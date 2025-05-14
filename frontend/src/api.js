9e912a96793192e7cf5eb42c4957bb1f14275765
Uditkishore <uditkishor4292@gmail.com>
2025-05-14 18:44:41 +0530

    minor fixes

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,4 +1,4 @@
 export const baseUrl_1 = "https://python.verifplay.com";
 // export const baseUrl = "http://localhost:3000";
 
-export const baseUrl= "https://login.verifplay.com/backend/";
+export const baseUrl= "https://login.verifplay.com/backend";
